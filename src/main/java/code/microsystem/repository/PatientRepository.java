2bcdbf5bba52c8d5c225197dad26bbcd76959078
RUTU <rutujamankar300@gmail.com>
2024-09-08 22:05:29 +0530

    Rutuja Commit 3

--- src/main/java/code/microsystem/repository/PatientRepository.java
+++ src/main/java/code/microsystem/repository/PatientRepository.java
@@ -7,10 +7,13 @@ import org.springframework.stereotype.Repository;
 import code.microsystem.entity.Patient;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     @Query("select p from Patient p where p.pname=?1 and p.age=?2")
     List<Patient> findByPnameAndAge(String pname, int age);
+//repo
+	Optional<Patient> findByPname(String pname);
 }
